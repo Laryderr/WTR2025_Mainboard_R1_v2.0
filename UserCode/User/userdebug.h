c55affd5820fe6f3d23379d4207e9eac5d83d8e8
Laryderr <136736314+Laryderr@users.noreply.github.com>
2025-08-30 13:22:03 +0800

    增加任务堆栈监测功能

--- UserCode/User/userdebug.h
+++ UserCode/User/userdebug.h
@@ -5,6 +5,37 @@ extern "C"{
 #endif 
 
 #include "usermain.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* 任务堆栈信息结构体 */
+typedef struct {
+    TaskHandle_t taskHandle;    // 任务句柄
+    char taskName[20];          // 任务名称
+    UBaseType_t stackSize;      // 任务分配的堆栈大小（单位：字）
+    UBaseType_t minRemainingStack; // 历史最小剩余堆栈（高水位线值）
+    uint8_t stackUsagePercent;  // 堆栈使用百分比
+} TaskStackInfo_t;
+
+/* 需要监测的任务列表ID */
+typedef enum {
+    TASK_DEFAULT = 0,
+    TASK_REMOTE_CTRL,
+    TASK_CHASSIS_CAN_MSG,
+    TASK_CHASSIS_CTRL,
+    TASK_UNITREE_UART_MSG,
+    TASK_CHASSIS_GYRO,
+    TASK_EXPANSION_EXECUTOR,
+    TASK_CHASSIS_ODOM,
+    TASK_HANDLE_SHOOT,
+    TASK_DEBUG,
+    TASK_MAIN,
+    TASK_IWDG,
+    TASK_MAX_NUM
+} TaskID_t;
+
+// extern TaskStackInfo_t taskStackInfo[TASK_MAX_NUM];
+
 
 
 
